85f40915966dda382d92a791183682ef0c021811
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-06-27 10:51:18 +0200

    header modificato-responsive

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,16 +1,11 @@
 <div class="header">
-    <div class="container container-header">
-        <div class="logo">
-            <img src="{{url('/images/logo.jpg')}}" alt="logo">
-        </div>
-            <nav>
-                <ul>
-                    <li><a href="#">Diventa un host</a></li>
-                    <li><a href="#">Proponi una esperienza</a></li>
-                    <li><a href="#">Assistenza</a></li>
-                    <li><a href="#">Accedi</a></li>
-                </ul>
-                <button type="button" name="button">Sign up</button>
-            </nav>
-    </div>
+  <a href="" class="logo"><img src="{{url('/images/logo.jpg')}}" alt="logo"></a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+      <li><a href="#">Diventa un host</a></li>
+      <li><a href="#">Proponi una esperienza</a></li>
+      <li><a href="#">Accedi</a></li>
+      <li><a href="#">Registrati</a></li>
+  </ul>
 </div>
